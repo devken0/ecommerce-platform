7deab06e6391680241799bb070d8e846ebfd3c50
devken0 <ken.anonym@anon.com>
2024-01-28 09:38:17 +0000

    Update frontend code change the application title to display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -65,7 +65,7 @@ function App() {
               <i className="fa fa-bars"></i>
             </button>
             <Link className="brand" to="/">
-              amazona
+              amazona.ph
             </Link>
           </div>
           <div>
